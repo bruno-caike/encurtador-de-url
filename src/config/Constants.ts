e3bc550ab830c22ecaa3145c1cd1c0a7488d8e0c
brunocaike <brunokf1512@gmail.com>
2022-03-08 20:45:28 -0300

    criado um encurtador de URL

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://bruno_caike:99579200@cluster0.t1zl3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
